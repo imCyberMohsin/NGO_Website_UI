a85136ca76f4aa0c8175862b202df5c84f1a3e1f
imCyberMohsin <mohsinansari7033@gmail.com>
2024-04-20 18:26:57 +0530

    landing, home, nav updated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 //? Home route
-router.get('/', (req, res)=> [
-    res.render('index')
-])
+router.get('/', (req, res) => {
+    res.render('homePage')
+});
+
+//? News route
+router.get('/news', (req, res) => {
+    res.render('newsPage')
+})
 
 module.exports = router;
