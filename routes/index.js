5e643e731b1c0e220e4ba9ddce2645e62eaf9ae5
imCyberMohsin <mohsinansari7033@gmail.com>
2024-04-23 16:37:25 +0530

    gallery page added

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,9 @@ router.get('/donate', (req, res) => {
     res.render('donatePage')
 })
 
+//? Gallery route
+router.get('/gallery', (req, res) => {
+    res.render('galleryPage')
+})
+
 module.exports = router;
