0b503fbd3f961a2ce1bf419960c32733dc1bcfda
imCyberMohsin <mohsinansari7033@gmail.com>
2024-04-25 04:21:26 +0530

    theme changed

--- public/js/smoothScroll.js
+++
@@ -1,8 +0,0 @@
-//! Smooth Scrolling Script using Locmotive
-// document.addEventListener('DOMContentLoaded', () => {
-//     const scroll = new LocomotiveScroll({
-//         el: document.querySelector('#smooth-div'),
-//         smooth: true,
-//         direction: 'vertical',
-//     });
-// });
